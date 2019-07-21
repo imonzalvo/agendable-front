293785abc7e820ae35c5019e8e358c77d726602c
Ignacio Monzalvo <ignaciomonzalvo@gmail.com>
2019-07-21 14:56:58 -0300

    change auth

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -28,4 +28,9 @@ const Home = (): React.ReactNode => (
   </PageHeaderWrapper>
 );
 
-export default withAuthenticator(Home, true);
+const rrr = {
+  google_client_id: '997269797206-ir9cl70i56luah9sav5m76d8u1g3f2jp.apps.googleusercontent.com', // Enter your google_client_id here
+  facebook_app_id: '381076525880234', // Enter your facebook_app_id here
+};
+
+export default withAuthenticator(Home, { federated: rrr });
