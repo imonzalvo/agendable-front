6eb69aa99d63081012812e077a985ef1e933765b
Diego Pizzarello <diegopizzarello@gmail.com>
2019-11-01 00:52:15 -0300

    setup storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,4 @@
 import { configure } from '@storybook/react';
 
-function loadStories() {
-  require('../stories/index.js');
-  // You can require as many stories as you need.
-}
-
-configure(loadStories, module);
+// We should create stories inside components' folder
+configure(require.context('../stories', true, /\.js$/), module);
