2572f618bb55ff3598debe6bb06086a0888b2eff
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.2>
2023-05-29 00:34:40 -0300

    Fix 4

--- src/utils/parsePathnameHandle.ts
+++ src/utils/parsePathnameHandle.ts
@@ -1,12 +1,22 @@
 export const parsePathnameHandle = (pathname: string) => {
   if (typeof pathname === 'string') {
     const pathnameArr = pathname.split('/');
-    if (pathnameArr.length >= 2 && pathnameArr[1] === 'a' && pathnameArr[2]) {
-      return pathnameArr[2];
+    if (pathnameArr.length >= 2) {
+      return pathnameArr[1];
     }
   }
   return null;
 };
+
+// export const parsePathnameHandle = (pathname: string) => {
+//   if (typeof pathname === 'string') {
+//     const pathnameArr = pathname.split('/');
+//     if (pathnameArr.length >= 2 && pathnameArr[1] === 'a' && pathnameArr[2]) {
+//       return pathnameArr[2];
+//     }
+//   }
+//   return null;
+// };
 
 // export const parsePathnameHandle = (pathname: string) => {
 //   if (typeof pathname === 'string') {
